d78b3e07398e3fd9c2938bbd864cb94f0f9048c8
Dev-scott <sadoscott25@gmail.com>
2024-09-07 22:56:27 +0100

    feat: work on navbar

---
+++ src/app/(landing)/layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import LandingPageNavbar from './_components/navbar'
+
+type Props = {}
+
+const LandingPageLayout = ({children}:{children:React.ReactNode}) => {
+  return (
+    <div className='flex flex-col container relative'>
+        
+        <LandingPageNavbar/>
+        {children}        
+        </div>
+  )
+}
+
+export default LandingPageLayout
